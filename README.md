128637580771c38d71cb386d61d51cf3ce8ad436
netrodslessa <netrods@gmail.com>
2020-06-02 16:46:36 -0300

    finalização da segunda aula

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nlw
+
 Semana Next Level week
---
+++ scripts/introducao.js
@@ -0,0 +1,52 @@
+// comments
+// document.write("Hello World!")
+// variáveis, tipos de dados
+// var myvar 
+// myvar = 'Hellow'
+// document.write(myvar)
+// const myconst = 'he'
+// document.write(myvar + myconst)
+
+//Objeto todos objetos possuem valores e funcionalidades
+// 
+// const pessoa = {
+//   altura: '1,80m',
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//    document.write('executar')
+    
+//   }
+// }
+// pessoa.correr()
+
+
+// vetor array
+// const colecaoDeBolinhas = ['blue', 'green', 1, {name: 'my Name'}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// funções - funcionalidades - atalhos - reuso de códigos
+//resgistrar função
+// function saMyName(name){
+//   document.write(name)
+// }
+// //usar função
+// saMyName('Douglas')
+// saMyName('Valesca')
+// saMyName(`Kyan`)
+
+// condicionais
+
+// const notaFinal = 4
+
+// if(notaFinal < 5){
+//   document.write('reprovado')
+// }else{
+//   document.write('Aprovado')
+// }
+
+//  repetições
+// for (i = 0; i < 10; i++){
+//   document.write(`<p>${i}</p>`)
+// }
