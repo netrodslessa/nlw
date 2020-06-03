f5f7d7b525ec7646c1bcef753f3891768a71d865
netrodslessa <netrods@gmail.com>
2020-06-03 16:19:07 -0300

    finalização da aula 2

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -25,8 +25,12 @@ function getCities(event) {
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
     fetch(url).then(res => res.json())
         .then(cities => {
+            // corrigi o bug limpando o campo cidade quando estado é trocado
+            citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+            citySelect.disabled = true
+            // fim da correção
             for (const city of cities) {
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
             // abilita o campo cidade
             citySelect.disabled = false;
@@ -36,3 +40,57 @@ function getCities(event) {
 
 document.querySelector("select[name=uf]")
     .addEventListener("change", getCities)
+
+// itens de coleta
+// pegar todos os li's
+const itemsToColect = document.querySelectorAll('.items-grid li')
+
+
+for (const item of itemsToColect) {
+    item.addEventListener('click', handleSelectedItems)
+}
+
+// pegar o campo escondido com os itens selecionados
+const collectedItems = document.querySelector('input[name=]items')
+
+// selecionando items dos itens de coleta
+let selectedItems = []
+
+// função de seleção de itens
+function handleSelectedItems(e) {
+    const itemLi = event.target
+    // adicionando uma classe ao objeto, isso ajuda a não precisar configurar o style por aqui.
+    // adicionar ou remover uma classe com Javascript
+    itemLi.classList.toggle('selected') //dentro do TOGGLE posso inserir ou remover qual classe preciso
+    // itemLi.classList.add => para somente adicionar
+    // itemLi.classList.remove => para somente remover
+
+    // dataset.id pega o valor do id
+    const itemId = itemLi.dataset.id
+
+    // verificar se tem itens selecionados, se sim, 
+    // pegar os itens selecionados
+    // alreadySelected = Já foi selecionado 
+    const alreadySelected = selectedItems.findIndex(item => {
+        const itemFound = item == itemId // isso sera true ou false
+        return itemFound
+    })
+
+    // se já estiver selecionado, tirar da seleção 
+    if (alreadySelected >= 0) {
+        // tirar da seleção
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        })
+        selectedItems = filteredItems
+    } else {
+        // se não estiver selecionado
+        //adicionar à seleção
+        selectedItems.push(itemId)
+    }
+    // console.log(selectedItems)mostra o que foi selecionado
+
+    // atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems;
+}
